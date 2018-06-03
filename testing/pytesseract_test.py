b855c2d67af4281597a093d8144ec74b7141044b
Erik <erikthomas146@gmail.com>
2018-06-03 19:12:51 +0100

    Image to text conversion test.

--- testing/pytesseract_test.py
+++ testing/pytesseract_test.py
@@ -11,7 +11,7 @@ from PIL import Image
     -- Create new environment variable called TESSDATA_PREFIX and point it to the parent tesseract directory
 """
 # can change path to image to scan for text
-image = cv2.imread("colour_of_magic.png", 0)
+image = cv2.imread("IMG_20180603_190644.jpg", 0)
 
 thresholding = input("Please enter the thresholding method to use: thresh or blur\n")
 
@@ -27,8 +27,12 @@ cv2.imwrite(filename, image)
 text = pytesseract.image_to_string(Image.open(filename))
 os.remove(filename)
 # TODO: write to a file with timestamp?
-print(text)
+if text is "":
+    print("No text detected, please try again...")
+else:
+    print(text)
 # can remove if not required, for testing
+image = cv2.resize(image, None, fx=0.2, fy=0.2)
 cv2.imshow("Image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
