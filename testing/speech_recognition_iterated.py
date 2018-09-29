ca07d17d5b664d400af041e8fb4504877f2eaf14
Fyrngarm <erikthomas146@gmail.com>
2018-09-29 17:17:34 +0100

    Finished refacatoring command-based actions

--- testing/speech_recognition_iterated.py
+++ testing/speech_recognition_iterated.py
@@ -2,10 +2,8 @@
 Author: Erik Thomas 28-09-18
 
 TODO: 
-    -- Store commands in .csv file with three columns:
-        - Type: greeting, behaviour etc.
-        - Command: the speech command related to each action
-        - Action: speech output or behaviour to perform, call relevant method
+    -- Somehow compact the if statements
+    -- Separate robot's actions into behaviour class
 """
 import time
 import random
@@ -28,13 +26,30 @@ class Speech():
         self.IP = ip
         # constant for all instances of the robot
         self.PORT = 9559
-        self.com_dict = pl.get_list(com_file, com_dir)
+        # TODO: change this to use pickle
+        self.com_dict = 
+                {"speech": 
+                    {"hello robbie", "fire the trebuchet", "what is the time",
+                    "how are you", "where are we", "what is this department",
+                    "what is your favourite pathway"},
+                "movement": 
+                    {"can you sit down", "stand", "can you walk forward",
+                    "come over here robbie", "can you walk backwards", 
+                    "are you tired robbie"},
+                "behaviours": 
+                    {"can you play the guitar", "why don't you do some exercise",
+                    "bless you"},
+                "dance": 
+                    {"do you feel like dancing", "show me your best dance moves",
+                    "do you know thriller"}
+                "stop": {"it is time to stop", "can you please stop that movement"} 
+                } 
         # checks move is a motion proxy
         if type(motion_proxy) is ALProxy:
             self.motion_proxy = motion_proxy
             raise Exception("DEBUG -- motion proxy not set when passed to Speech class") 
         self.tts = Other(self.IP, self.PORT)
-        self.bhv = Behaviour(bhv_file, bhv_dir)
+        self.bhv = Behaviour(self.motion_proxy, self.tts, bhv_file, bhv_dir)
         # setting up audio device for speech recognition
         self.mic = Microphone(log_dir)
         self.mic_enabled = self.mic.mic_check()
@@ -51,7 +66,7 @@ class Speech():
         
         while command != None:
             command = self.mic.audio_log()
-            self.commands(command)
+            self.check_command(command)
 
     def check_command(self, command):
         """
@@ -60,28 +75,98 @@ class Speech():
         Keyword arguments:
         command -- a string of the audio input for the robot
         """
-        # FIXME: needs to read based on command's category, and have an output string
         command = command.lower()
 
         for key in self.com_dict:
             if command in self.com_dict[key]:
-                self.run_command(key)
+                self.run_command(key, command)
             else:
                 self.tts.speak("I don't recognise that command!")
     
-    def run_command(self, key):
-        # FIXME: to be used with check_command, requires update
-        pass
-        
-    def greeting(self, command):
-        self.tts.speak(random.choice(self.bhv.greeting_list))
-        self.run_bhv(action=command)
+    def run_command(self, key, command):
+        """
+        The parent method to each type of action for the robot
+        all other methods that get the robot to act are called from here
+
+        Keyword arguments:
+        key -- the key of the command dictionary to check which sub-method to call
+        command -- used to trigger certain actions in the sub-methods
+        """
+        # checks whether to stop first for efficiency (doesn't have to check other dictionary keys)
+        if key == "stop":
+            self.motion_proxy.behaviour(behaviour_name="stop")
+        elif key == "speech":
+            self.run_speech(command)
+        elif key == "movement":
+            self.run_mv(command)
+        elif key == "behaviours":
+            self.bhv.run_bhv(command)
+        elif key == "dance":
+            self.bhv.run_dance(command)  
     
-    def emotion(self, command):
+    def greeting(self):
+        self.tts.speak(random.choice(self.bhv.greeting_list))
+
+    def emotion(self):
         self.tts.speak(random.choice(self.bhv.emotions_list))
 
-    def run_bhv(self, action):
-        self.motion_proxy.behaviour(behaviour_name=self.bhv.bhv_dict[action])
+    def run_speech(self, command):
+        """
+        Runs methods that only require the robot to speak
+
+        Keyword arguments:
+        command -- used to check what the robot should say
+        """
+        if command == "hello" or command == "hello robbie":
+            self.greeting()
+        elif command == "what is the time" or command == "what time is it":
+            # formats the current time into a speakable format for the robot
+            now = datetime.datetime.now()
+            hour = now.hour
+            if hour > 12:
+                hour -= 12
+                meridiem = 'pm'
+            else:
+                meridiem = 'am'
+            self.tts.speak("The time is {}:{} {}".format(hour, now.minute, meridiem))
+        elif command == "how are you" or command == "how are you robbie":
+            self.emotion()
+
+        elif command == "where are we":
+            self.tts.speak("We are at Edge Hill University, situated in the north-western town of Ormskirk")
+            
+        elif command == "what is this department":
+            self.tts.speak("this is the computer science department, which has a wide variety of courses!")
+            
+        elif command == "what is your favourite pathway":
+            self.tts.speak("BSc Robotics and Artificial Intelligence of course!")
+
+    def run_mv(self, command):
+        """
+        Runs primitive movement related methods for the robot
+
+        Keyword arguments:
+        command -- determines what movement the robot will perform
+        """
+        if command == "can you sit down":
+            self.tts.speak("Time for a sit down!")
+            self.motion_proxy.posture("Sit")
+            
+        elif command == "stand":
+            self.tts.speak("Standing up!")
+            self.motion_proxy.posture("StandInit")
+            
+        elif command == "can you walk forwards" or command == "come over here robbie":
+            self.tts.speak("Get ready to catch me!")
+            self.motion_proxy.walk_fw_set()
+            
+        elif command == "can you walk backwards":
+            self.tts.speak("Get ready to catch me!")
+            self.movmotion_proxye.walk_bk_set()
+            
+        elif command == "are you tired robbie":
+            self.tts.speak("yawn... time for a lie down")
+            self.motion_proxy.posture("LyingBack") 
 
 class Microphone():
     def __init__(self, audio_log):
@@ -129,8 +214,8 @@ class Microphone():
         audio_log -- the most recent sentence that has been detected as a string
         """
         data = []
-        curr_time = time.time()
-        time_struct = time.localtime(curr_time)
+        t_now = time.time()
+        t_struct = time.localtime(t_now)
 
         with sr.Microphone(self.MIC) as src:
             audio = self.s_rec.listen(src, timeout=self.T_OUT, phrase_time_limit=self.T_LIMIT)
@@ -138,7 +223,7 @@ class Microphone():
 
         try:
             audio_log = self.s_rec.recognize_google(audio)
-            print("at {} user said {}".format(time.asctime(time_struct), audio_log))
+            print("at {} user said {}".format(time.asctime(t_struct), audio_log))
             data.append(audio_log)
 
             with open(self.audio_log, 'a') as log:
@@ -158,7 +243,12 @@ class Microphone():
         return audio_log
 
 class Behaviour():
-    def __init__(self, f_name, _dir = ''):
+    def __init__(self, motion_proxy, tts, f_name, _dir = ''):
+        self.BHV_LBL = "behaviour"
+        self.DNC_LBL = "dance"
+        self.motion_proxy = motion_proxy
+        self.tts = tts
+
         self.bhvs = self.deserialise_behaviours(f_name, _dir)
         self.greeting_list = next(self.bhvs)
         self.emotions_list = next(self.bhvs)
@@ -166,5 +256,57 @@ class Behaviour():
         self.bhv_dict = next(self.bhvs)
 
     def deserialise_behaviours(self, f_name, _dir):
+        """
+        Unpickles a given .pkl file and stores the containers in a generator
+
+        Returns:
+        items -- a generator containing dictionaries and lists related to behaviours
+        """
         items = pl.get_list(f_name, _dir)
         return items
+
+    def call_act(self, action, act_type=''):
+        """
+        Calls the motion_proxy's behaviour method using a given action
+
+        Keyword arguments:
+        action -- key of the respective dictionary that contains the path to the behaviour on the robot
+        act_type -- either behaviour or dance, depending on the action to be performed
+        """
+        if act_type == "behaviour":
+            self.motion_proxy.behaviour(behaviour_name=self.bhv_dict[action])
+        elif act_type == "dance":
+            self.motion_proxy.behaviour(behaviour_name=self.dance_dict[action])
+
+    def run_bhv(self, command):
+        """
+        Gets the robot to run one of its installed behaviours
+
+        Keyword arguments:
+        command -- the behaviour for the robot to perform
+        """
+        if command == "can you play the guitar":
+            self.call_act("air guitar", self.BHV_LBL)
+        elif command == "why don't you do some exercise" or command == "can you do push-ups":
+            self.call_act("pushups", self.BHV_LBL)
+        elif command == "bless you":
+            self.call_act("sneeze", self.BHV_LBL)
+
+    def run_dance(self, command):
+        """
+        Makes the robot peform various dances
+
+        Keyword arguments:
+        command -- the dance for the robot to perform
+        tts_proxy -- the text to speech proxy to allow the robot to speak
+        """
+        if command == "do you know thriller":
+            self.tts.speak("perhaps I do...")
+            # robbie needs a little break to psyche himself up...
+            time.sleep(1)
+            self.call_act("thriller", self.DNC_LBL)
+        elif command == "do you feel like dancing":
+            _, dance = random.choice(self.bhv.dance_dict.keys())
+            self.call_act(dance, self.DNC_LBL)    
+        elif command == "show me your best dance moves":
+            self.call_act("caravan palace", self.DNC_LBL)
