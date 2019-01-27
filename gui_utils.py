d79afc64441e773ba7f68140e6e65537555f83bc
Fyrngarm <erikthomas146@gmail.com>
2019-01-27 16:30:32 +0000

    New method to toggle label colour and text added

--- gui_utils.py
+++ gui_utils.py
@@ -8,3 +8,12 @@ def toggle_checkbox_state(checkbox, toggle_text):
         checkbox.setText(toggle_text[1])
     elif checkbox.text() == toggle_text[1]:
         checkbox.setText(toggle_text[0])
+
+def toggle_label_status(label, text_options, colours):
+    if label.text() == text_options[0]:
+        idx = 1
+    elif label.text() == text_options[1]:
+        idx = 0
+        
+    label.setStyleSheet("background-color: rgb{}".format(colours[idx]))
+    label.setText(text_options[idx])
